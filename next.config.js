fbfd775342e78eea66170eb1ec64207f749bc167
jicjjang <jicjjang12@gmail.com>
2019-09-14 20:51:46 +0900

    chore

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 const withSass = require('@zeit/next-sass');
 const postContents = require('./contents/posts');
 
-const postPages = postContents.reduce((postPages, post) => {
-  return post.layout === 'post'
-    ? Object.assign({}, postPages, {
-        [post.path]: {
-          page: '/post',
-          query: { id: post.path.split('/post/')[1] }
-        }
-      })
-    : postPages;
-}, {});
+const postPages = postContents.reduce(
+  (postPages, post) =>
+    post.layout === 'post'
+      ? Object.assign({}, postPages, {
+          [post.path]: {
+            page: '/post',
+            query: { id: post.path.split('/post/')[1] }
+          }
+        })
+      : postPages,
+  {}
+);
 
 module.exports = withSass({
   cssModules: true,
