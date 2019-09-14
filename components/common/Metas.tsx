5322e3fe2bab516396bf22fd0b7bad1ca50f3b43
jicjjang <jicjjang12@gmail.com>
2019-09-14 20:07:22 +0900

    Append cache metas

--- components/common/Metas.tsx
+++ components/common/Metas.tsx
@@ -12,6 +12,9 @@ const Metas = () => {
       <link rel="alternate" hrefLang="x-default" href={baseUrl} />
       <link rel="alternate" hrefLang="ko-kr" href={baseUrl} />
 
+      <meta http-equiv="cache-control" content="Public" />
+      <meta http-equiv="Expires" content="1800" />
+
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="This blog has been used since 2019 using Nextjs with Reactjs" />
